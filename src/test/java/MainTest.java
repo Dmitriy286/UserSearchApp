aa39d2cca6ef71fa11b0b885b9ff7b0e49e42e49
Dmitry286 <DShishlov@gmail.com>
2022-11-13 20:59:58 +0300

    init

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,26 @@
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testReadFile() {
+    }
+
+    public void testScan() {
+    }
+
+    public void testCreateExcludeIdsList() {
+    }
+
+    public void testFind() {
+    }
+
+    public void testTestFind() {
+    }
+}
